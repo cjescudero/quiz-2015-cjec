2ddf5dcc2ebf132622636860cd4860815b45d95f
cjescudero <cjescudero@gmail.com>
2015-06-09 17:32:48 +0200

    Creación de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -15,7 +15,7 @@ exports.load = function(req, res, next, quizId) {
 // GET /quizes
 exports.index = function(req,res) {
   var busqueda = req.query.search || "";
-  
+
   busqueda=busqueda.replace(/\s+/g,'%');
   console.log("Filtro: " + busqueda);
   models.Quiz.findAll({
@@ -39,4 +39,22 @@ exports.answer = function(req,res) {
   } else {
     res.render('quizes/answer', {quiz: req.quiz, respuesta: 'Incorrecto'});
   };
+};
+
+// GET /quizes/new
+exports.new = function(req, res) {
+  var quiz = models.Quiz.build( // crea objeto quiz
+    {pregunta: "Pregunta", respuesta: "Respuesta"}
+  );
+
+  console.log(quiz.pregunta +' / '+quiz.respuesta);
+  res.render('quizes/new', {quiz: quiz});
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+  var quiz = models.Quiz.build( req.body.quiz );
+        quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+          res.redirect('/quizes');
+        })
 };
