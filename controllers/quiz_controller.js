956dd2968daef5dbe3fc37cb299847e9802f3876
cjescudero <cjescudero@gmail.com>
2015-06-10 20:55:58 +0200

    Validación de entradas
    Se ha incorporado nueva versión de sequelize

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,14 +2,14 @@ var models=require('../models/models.js');
 
 // Autoload
 exports.load = function(req, res, next, quizId) {
-  models.Quiz.find(quizId).then(
+  models.Quiz.findById(quizId).then(
     function(quiz) {
       if (quiz) {
         req.quiz=quiz;
         next();
       } else { next(new Error('No existe quizId=' + quizId));}
     }
-  ).catch(function(error) {next(error);});
+  );
 };
 
 // GET /quizes
@@ -22,23 +22,29 @@ exports.index = function(req,res) {
       where: ['pregunta like ?', '%'+busqueda+'%'],
       order: "pregunta ASC"
       }).then(function(quizes) {
-    res.render('quizes/index.ejs', {quizes: quizes});
+    res.render('quizes/index.ejs', {quizes: quizes, errors: []});
   })
 };
 
 
 // GET /quizes/show
 exports.show = function(req,res) {
-  res.render('quizes/show',{quiz: req.quiz});
+  res.render('quizes/show',{quiz: req.quiz, errors: []});
 };
 
 // GET /quizes/answer
 exports.answer = function(req,res) {
+  var resultado = 'Incorrecto';
   if (req.query.respuesta === req.quiz.respuesta) {
-    res.render('quizes/answer', { quiz: req.quiz, respuesta: 'Correcto'});
-  } else {
-    res.render('quizes/answer', {quiz: req.quiz, respuesta: 'Incorrecto'});
-  };
+    resultado = 'Correcto';
+  }
+  res.render(
+    'quizes/answer',
+    { quiz: req.quiz,
+      respuesta: resultado,
+      errors: []
+    }
+  );
 };
 
 // GET /quizes/new
@@ -47,14 +53,26 @@ exports.new = function(req, res) {
     {pregunta: "Pregunta", respuesta: "Respuesta"}
   );
 
-  console.log(quiz.pregunta +' / '+quiz.respuesta);
-  res.render('quizes/new', {quiz: quiz});
+  res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 // POST /quizes/create
 exports.create = function(req, res) {
   var quiz = models.Quiz.build( req.body.quiz );
-        quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-          res.redirect('/quizes');
-        })
+  console.log('--->');
+  console.log(req.body.quiz);
+
+  quiz
+  .validate()
+  .then(
+    function(err){
+      if (err) {
+        res.render('quizes/new', {quiz: quiz, errors: err.errors});
+      } else {
+        quiz // save: guarda en DB campos pregunta y respuesta de quiz
+        .save({fields: ["pregunta", "respuesta"]})
+        .then( function(){ res.redirect('/quizes')})
+      }      // res.redirect: Redirección HTTP a lista de preguntas
+    }
+  ).catch(function(error){next(error)});
 };
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors: []});
 });
 
 // Autoload de quizId
@@ -19,7 +19,7 @@ router.get('/quizes/new', quizController.new);
 router.post('/quizes/create', quizController.create);
 
 router.get('/author', function(req,res) {
-    res.render('authors');
+    res.render('authors', {errors: []});
 });
 
 module.exports = router;
