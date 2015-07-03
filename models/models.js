98b3c59c8a8509475f6740305fd57d8f321e0912
cjescudero <cjescudero@gmail.com>
2015-07-03 18:37:54 +0200

    Comentarios

--- models/models.js
+++ models/models.js
@@ -28,7 +28,18 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 
 // Importar definición de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+
+// Importar definicion de la tabla Comment
+var comment_path = path.join(__dirname,'comment');
+var Comment = sequelize.import(comment_path);
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
+
+// exportar tablas
 exports.Quiz = Quiz;
+exports.Comment = Comment; 
 
 sequelize.sync().then(function() {
   Quiz.count().then(function (count){
